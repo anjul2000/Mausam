13d86b0c5fe3f4863f83d332492ea6b0c370d205
Anjul Singh <anjul2000@yahoo.in>
2020-11-18 18:42:43 +0530

    Initial commit

---
+++ app/src/main/java/e/anjul/singh/mausam/MainActivity.java
@@ -0,0 +1,16 @@
+package e.anjul.singh.mausam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //cda4f181a1adecf2dee0db6e9e6e54e7
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
